3301683b0bdc60a6a992f845ca0133509fcf177c
nijiaqi <Aa12345678>
2018-05-03 17:56:55 +0800

    aaaaaaaa

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -10,6 +10,7 @@ public class TestController {
     public  String test() {
         System.out.print("aaa");
         System.out.print("lalala");
+        System.out.print("lalala");
         return "aaa";
     }
 
