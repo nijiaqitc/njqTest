056a1e747e6fc890f3ad502323955ee0f872bf69
nijiaqi <Aa12345678>
2018-05-03 18:12:30 +0800

    冲突修改sdsdsd

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,9 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * ddd
+ */
 @SpringBootApplication
 public class DemoApplication {
 
--- src/main/java/com/example/demo/Test2Controller.java
+++ src/main/java/com/example/demo/Test2Controller.java
@@ -1,4 +1,7 @@
 package com.example.demo;
 
+/**
+ * ssss
+ */
 public class Test2Controller {
 }
